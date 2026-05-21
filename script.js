04f7e6ea14769aed7cb77b7069537f6c6f04d3be
Lecornu Paul <paul.lecornu08@gmail.com>
2026-05-21 09:53:30 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+/* Exo1
+var C = 30
+var F = (C*9/5+32)
+console.log(C);
+console.log(F);
+*/
+
+/* Exo2
+var l = 5
+var L = 5
+console.log(l*L);
+*/
+
+/* Exo3
+var prenom = "Paul"
+var nom = "Lecornu"
+console.log(prenom+" "+nom);
+*/
+
+/* Exo4
+var HT = 10
+var TVA = 20
+var TTC = HT + HT*20/100 
+console.log(TTC);
+*/
+
+/* Exo5
+((4 >= 6) || ("herbe"!="verte"))&& !(((12*2)==144 && true));
+*/
+
+/* Exo6
+var poids = 60
+var taille = 1.83
+var IMC = poids / (taille*taille)
+console.log(IMC.toFixed(2));
+*/
+
+/* Exo7
+var min = 10
+function commande(total) {
+    if (total >= min) {
+        result = "Livraison Gratuite !";
+    } else {
+        result = "La livraison coutera X euros.";
+    }
+    return result
+}
+
+console.log(commande(9));
+*/
+
